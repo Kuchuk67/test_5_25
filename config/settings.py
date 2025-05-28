a028e9937e492327b5af4865be278966d27a67ee
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-05-28 13:17:35 +0300

    4

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "89.169.166.244"]
 
 
 # Application definition
